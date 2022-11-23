4a7513b10e909e0673cb5475bd7da97761885bb7
John Macaday <johnelsman.macaday@gmail.com>
2022-11-23 20:52:02 +0800

    added Header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img src="/logo-pokemon.png" alt="pokemon logo" />
+    </div>
+  );
+};
+
+export default Header;
